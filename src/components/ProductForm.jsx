41c781353f399e130096cd7bc550f8d110d59ce8
Yoisell Cervantes <ycervantes@e-contact.cl>
2022-10-26 13:07:35 -0300

    fisrt mern app

---
+++ src/components/ProductForm.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import axios from 'axios';
+
+
+
+
+function ProductForm() {
+    const [tittle, setTittle] = useState("")
+    const [price, setPrice] = useState("")
+    const [description, setDescription] = useState("")
+
+    const onSubmitHandler = e =>{
+        e.preventDefault();
+        axios.post('http://localhost:8001/api/product', {
+            tittle,
+            price,
+            description
+        })
+        .then(response=>console.log("Response:", response))
+        .catch(err=>console.log("Error:", err))
+    }
+
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+        <p>
+            <label>Tittle</label>
+            <input  type="text" onChange={e=>setTittle(e.target.value)}/>
+        </p>
+        <p>
+            <label>Price</label>
+            <input  type="text" onChange={e=>setPrice(e.target.value)}/>
+        </p>
+        <p>
+            <label>Description</label>
+            <input  type="text" onChange={e=>setDescription(e.target.value)}/>
+        </p>
+        <input type="submit" />
+
+    </form>
+  )
+}
+
+export default ProductForm
---
+++ src/views/Main.jsx
@@ -0,0 +1,14 @@
+
+import ProductForm from '../components/ProductForm';
+
+function Main() {
+    
+  return (
+    <div>
+           <ProductForm />
+        </div>
+  )
+}
+
+export default Main
+
